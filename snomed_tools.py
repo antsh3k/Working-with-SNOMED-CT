9b78f7769f88a289616358f7d6c6b2075ab1efcb
Anthony Shek <antsh3k@users.noreply.github.com>
2020-10-05 18:48:01 +0100

    initial snomed tools commit

---
+++ snomed_tools.py
@@ -0,0 +1,49 @@
+# Incomplete tool set
+
+class Snomed:
+    def __init__(self, input_dict):
+        self.snomed2name = input_dict
+
+    def sctid2name(self, sctid):
+        return self.snomed2name[sctid][0]
+
+    def name2sctid(self, name):
+
+    # def sctid2syn
+
+    def sctid2tlc(self, sctid):
+        return self.snomed2name[sctid][2]
+
+
+class Snomed_pt2ch():
+    def __init__(self, input_dict):
+        self.pt2ch = input_dict
+
+    def get_children(self, sctid):
+        """
+        Retrieve one layer children terms
+        :param sctid: SCTID
+        :return: Next level children terms
+        """
+        result = []
+        result.append
+        return result
+
+
+    def get_all_children(self, sctid):
+        """
+        Get all children of a snomed term
+        :param snomed: SCTID
+        :return: All snomed children
+        """
+        result = []
+        stack = [sctid]
+        while len(stack) != 0:
+            current_snomed = stack.pop()
+            current_snomed_parent = self.pt2ch.get(current_snomed, [])
+            stack.extend(current_snomed_parent)
+            result.append(current_snomed)
+            result = list(set(result))
+        return result
+
+
